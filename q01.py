6932d8bb6911e9fa48ed2bede1dfb3a42565c63d
Carolina <cahenaor@unal.edu.co>
2019-05-23 21:39:17 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -3,14 +3,13 @@
 ## de la columna _c1 de la tabla tbl0
 ## 
 import pandas as pd
+
 import numpy as np
 
 df = pd.read_csv("tbl0.tsv", sep="\t")
 
-df2 = df.copy()
+df.groupby('_c1').count() 
 
-df2 = df2.groupby('_c1').count()
+df.groupby('_c1').count()['_c0']
 
-df2 = df2.groupby('_c1').count()['_c0']
-
-print(df2)
+print(df)
