9aa30f5c7d956c21affc4ad988096cc3444ad708
Carolina <cahenaor@unal.edu.co>
2019-05-27 21:22:59 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -4,12 +4,9 @@
 ## 
 ## ejecutar = python3 q01.py
 import pandas as pd
-import numpy as np
-df = pd.read_csv("tbl0.tsv", sep="\t")
-df2 = df.copy()
 
-df2 = df2.groupby('_c1').mean()
+df0 = pd.read_csv("tbl0.tsv", sep="\t")
 
-df2 = df2.groupby('_c1').mean()['_c2']
+df0 = df0.groupby('_c1').mean()['_c2']
 
-print(df2)
+print(df0)
