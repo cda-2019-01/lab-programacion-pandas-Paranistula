629aa1733676fb7eef026ab9ea5a61749348e4aa
Carolina <cahenaor@unal.edu.co>
2019-05-27 21:23:45 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -4,5 +4,15 @@
 ## de la tabla tbl1.tsv
 ## 
 import pandas as pd
-import numpy as np
-df = pd.read_csv("tbl0.tsv", sep="\t")
+
+df1 = pd.read_csv("tbl1.tsv", sep="\t")
+
+dfaux = df1.groupby('_c0')['_c4'].apply(list)
+
+df = pd.DataFrame()
+df['_c0'] = dfaux.keys()
+df['lista'] = [elem for elem in dfaux]
+
+df['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in df['lista']]
+
+print(df)
